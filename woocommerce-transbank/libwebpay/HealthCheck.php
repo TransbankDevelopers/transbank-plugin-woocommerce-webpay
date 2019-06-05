c69e74ebbad7e12d37bbcdc5df66a4fe7536fd46
Gonzalo Castillo Fajardo <goncafa@gmail.com>
2019-06-05 16:44:34 -0400

    Fix plugin URL to create PDF link

--- woocommerce-transbank/libwebpay/HealthCheck.php
+++ woocommerce-transbank/libwebpay/HealthCheck.php
@@ -80,7 +80,7 @@ class HealthCheck {
 
     // valida version de php
     private function getValidatephp(){
-        if (version_compare(phpversion(), '7.1.25', '<=') and version_compare(phpversion(), '5.5.0', '>=')) {
+        if (version_compare(phpversion(), '7.2.19', '<=') and version_compare(phpversion(), '5.5.0', '>=')) {
             $this->versioninfo = array(
                 'status' => 'OK',
                 'version' => phpversion()
