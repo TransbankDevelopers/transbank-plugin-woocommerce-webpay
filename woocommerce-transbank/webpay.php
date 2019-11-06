6e65250eb00c6d19d9496b4af4866a0c33bcbf6c
Kyberal <55935685+Kyberal@users.noreply.github.com>
2019-11-06 14:18:26 -0300

    Fix admin css
    Fixes administration css loading by using "plugin_dir_url( __DIR__ )" instead of "plugin_dir_path( __FILE__ )" for css files, it will now output the correct URL.
    Fixed "tbk.css" typo.

--- woocommerce-transbank/webpay.php
+++ woocommerce-transbank/webpay.php
@@ -292,8 +292,8 @@ function woocommerce_transbank_init() {
             ?>
             <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 
-			<link href="<?php echo plugin_dir_path( __FILE__ ) ?>css/bootstrap-switch.css" rel="stylesheet">
-			<link href="<?php echo plugin_dir_path( __FILE__ ) ?>ccss/tbk.css" rel="stylesheet">
+			<link href="<?php echo plugin_dir_url( __DIR__ ) ?>css/bootstrap-switch.css" rel="stylesheet">
+			<link href="<?php echo plugin_dir_url( __DIR__ ) ?>css/tbk.css" rel="stylesheet">
 
 			<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 			<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
