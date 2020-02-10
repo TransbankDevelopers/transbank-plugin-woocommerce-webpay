252e0bd2b4eab48e373dfc4101b30cb30a22f712
mpazvergara <35225390+mpazvergara@users.noreply.github.com>
2020-02-10 16:28:34 -0300

    Remove php upper limit
    Co-Authored-By: Gonzalo De Spirito <gonzunigad@gmail.com>

--- woocommerce-transbank/libwebpay/HealthCheck.php
+++ woocommerce-transbank/libwebpay/HealthCheck.php
@@ -83,7 +83,7 @@ class HealthCheck {
 
     // valida version de php
     private function getValidatephp(){
-        if (version_compare(phpversion(), '7.5', '<') and version_compare(phpversion(), '5.5.0', '>=')) {
+        if (version_compare(phpversion(), '5.5.0', '>=')) {
             $this->versioninfo = array(
                 'status' => 'OK',
                 'version' => phpversion()
