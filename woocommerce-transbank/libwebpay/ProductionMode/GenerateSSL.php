b8dbb8beb3dd9fadc506cb41a13cd9104af040f7
SaenGate <saengate@gmail.com>
2020-03-11 09:50:23 -0300

    Fix generate certificates

--- woocommerce-transbank/libwebpay/ProductionMode/GenerateSSL.php
+++ woocommerce-transbank/libwebpay/ProductionMode/GenerateSSL.php
@@ -6,31 +6,21 @@ require_once dirname(__FILE__, 2) . "/LogHandler.php";
 class GenerateSSL
 {
     // shell comando php -m | grep ssl
-    function __construct() {
+    function __construct($commonName) {
         $this->log = new LogHandler();
 
-        $this->dn = array(
-            "countryName" => "CL",
-            "stateOrProvinceName" => "Santiago",
-            "localityName" => "Santiago",
-            "organizationName" => "Transbank",
-            "organizationalUnitName" => "",
-            "commonName" => "",
-            "emailAddress" => "example@example.com"
-        );
         $this->bits = 2048;
         $this->extraArgs = array('digest_alg' => 'sha256');
         $this->days=1460;
-        $this->password = 'mypassword';
+        $this->password = "";
 
-        // output keys
-        $this->csrout = null;
-        $this->certout = null;
-        $this->pkeyout = null;
+        $this->dn = array(
+            "commonName" => $commonName,
+        );
     }
 
     private function __generate_ssl(){
-        
+
         // Generate a new private (and public) key pair
         $privkey = openssl_pkey_new(array(
             "private_key_bits" => $this->bits,
@@ -54,15 +44,23 @@ class GenerateSSL
         );
         
         // Save your private key, CSR and self-signed cert for later use
-        openssl_csr_export($csr, $this->csrout);
-        openssl_x509_export($x509, $this->certout);
-        openssl_pkey_export($privkey, $this->pkeyout, $this->password);
+        openssl_csr_export($csr, $csrout);
+        openssl_x509_export($x509, $certout);
+        openssl_pkey_export($privkey, $pkeyout, $this->password);
+
+        // output keys
+        return array(
+            'csrout' => $csrout,
+            'certout' => $certout,
+            'pkeyout' => $pkeyout,
+            'commerce_code' => $this->dn['commonName'],
+        );
 
     }
 
     public function generate(){
         try {
-            $this->__generate_ssl();
+            $result = $this->__generate_ssl();
         } catch(Exception $e) {
             $result = array(
                 "error" => 'Error al generar los certificados',
@@ -70,18 +68,35 @@ class GenerateSSL
             );
             $this->log->logError(json_encode($result));
         }
+        return $result;
     }
 
     public function getFullResume() {
-        $this->generate();
+        return json_encode($this->generate());
+    }
 
-        $this->fullResume = array(
-            'csrout' => $this->csrout,
-            'certout' => $this->certout,
-            'pkeyout' => $this->pkeyout,
+    // Ajax call method
+    public static function getSSL() {
+        $self = new GenerateSSL($_POST["commerce_code"]);
+        $result = $self->generate();
+        if ($result) {
+            if (!empty($result["error"]) && isset($result["error"])) {
+                $status = 'Error';
+            } else {
+                $status = 'OK';
+            }
+        } else {
+            if (array_key_exists('error', $result)) {
+                $status =  "Error";
+            }
+        }
+        $response = array(
+            'status' => $status,
+            'message' => preg_replace('/<!--(.*)-->/Uis', '', $result)
         );
-        return json_encode($this->fullResume);
+        ob_clean();
+        echo json_encode($response);
+        wp_die();
     }
 }
-
 ?>
